5d105b77857cec9dcac059938f8d2569b68e6071
Young998904 <like0998904@gmail.com>
2022-07-28 11:31:03 +0900

    Add ObjectMapper__jsonStrToObj Test to AppTest

--- src/main/java/com/ll/exam/utill/Ut.java
+++ src/main/java/com/ll/exam/utill/Ut.java
@@ -2,14 +2,25 @@ package com.ll.exam.utill;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.exam.article.dto.ArticleDto;
 
 public class Ut {
+    private static final ObjectMapper om;
+    static {
+        om = new ObjectMapper();
+    }
     public static class json {
         public static String toStr(Object obj, String defaultValue) {
-            ObjectMapper om = new ObjectMapper();
-
             try {
                 return om.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+
+        public static Object toObj(String jsonStr, Class cls, Object defaultValue) {
+            try {
+                return om.readValue(jsonStr, cls);
             } catch (JsonProcessingException e) {
                 return defaultValue;
             }
